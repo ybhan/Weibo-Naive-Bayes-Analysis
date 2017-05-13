a0c22d7d51dc9f386bad851a91a1ec6f333d5d07
Dylan-GAO <gaojingbo@gaojingbodeMacBook-Pro.local>
2017-05-13 10:27:45 +0800

    加粗了玫瑰图线条

--- general_statistics.m
+++ general_statistics.m
@@ -44,20 +44,21 @@ clear tmp h h_x h_y h_max;
 % plot the ROSE graph of the distribution of PROVINCE
 
 pro_size = max([data.province]) - min([data.province]) + 1;
-[pro_t, pro_r] = rose([data.province] / pro_size * 2 * pi, pro_size);
-figure;
-pax = polaraxes;
-polarplot(pro_t, pro_r);
-pax.ThetaTick = 5 : 10 : 360;
-pax.ThetaTickLabel = {...
+pro_name = {...
     'Others', 'Zhejiang', 'Shanxi', 'Jiangxi', 'Shanghai', 'Tianjin',...
     'Fujian', 'Liaoning', 'Guangdong', 'Beijing', 'Shandong', 'Heilongjiang', ...
     'Hebei', 'Anhui', 'Jiangsu', 'Hainan', 'Taiwan', 'Hunan', 'Sichuan',...
     'Shannxi', 'Jilin', 'Henan', 'Xinjiang', 'Hubei', 'Guangxi', 'Overseas',...
     'Chongqing',  'Neimeng', 'Gansu', 'Hongkong', 'Macao', 'Guizhou', 'Yunnan',...
     'Tibet', 'Qinghai', 'Ningxia'};
+[pro_t, pro_r] = rose([data.province] / pro_size * 2 * pi, pro_size);
+figure;
+pro_h = polarplot(pro_t, pro_r);
+set(pro_h, 'LineWidth', 1.3);
+set(gca, 'ThetaTick', 5 : 10 : 360);
+set(gca, 'ThetaTickLabel', pro_name);
 title('the Distribution of Province');
-clear h pax pro_r pro_size pro_t;
+clear pro_r pro_size pro_t pro_name pro_h;
 
 % tweet_from
 tmp = [data.tweet];
