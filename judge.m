08d200312dc8edb56c0bae6131b1c454901c90e1
Dylan-GAO <gaojingbo@gaojingbodeMacBook-Pro.local>
2017-05-14 19:26:03 +0800

    bug

--- judge.m
+++ judge.m
@@ -13,13 +13,13 @@ function P = judge(Tr_data, one)
     tc = one.tweet_count;
     interval = AverageIntervalOfTweets(one);
 
-    tmp = Tr_data_A([Tr_data_A.tweet_count] >= tc);
-    tmp = tmp(AverageIntervalOfTweets(tmp) >= interval);
+    tmp = Tr_data_A([Tr_data_A.tweet_count] <= tc);
+    tmp = tmp(AverageIntervalOfTweets(tmp) <= interval);
     P_1 = size(tmp, 2) / size(Tr_data, 2);
     P_2 = size(tmp, 2) / size(Tr_data_A, 2);
 
-    tmp = Tr_data_NA([Tr_data_NA.tweet_count] >= tc);
-    tmp = tmp(AverageIntervalOfTweets(tmp) >= interval);
+    tmp = Tr_data_NA([Tr_data_NA.tweet_count] <= tc);
+    tmp = tmp(AverageIntervalOfTweets(tmp) <= interval);
     P_3 = size(tmp, 2) / size(Tr_data_NA, 2);
 
     P = P_1 / ((P_2 * P_A) + (P_3 * P_NA));
