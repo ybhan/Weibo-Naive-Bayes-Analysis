9dce1c290e6c01f21648d8acc64b1625df23c4b0
Dylan-GAO <gaojingbo@gaojingbodeMacBook-Pro.local>
2017-05-25 15:41:29 +0800

    map注释

--- Map/map.m
+++ Map/map.m
@@ -1,22 +1,33 @@
 % May 24, 2017
 % Edited by Jingbo Gao
 
-% plot a china MAP and use the depth of color to illustrate the
-% distribution of active users
+% plot a China MAP and use the depth of color to illustrate the
+% distribution of active users & all users. The relavant China map data is 
+% downloaded from National Geomatics Center of China(NGCC)'s official
+% website.
 
-load ../Weibodata.mat
-load ../Active_users.mat
-load chinese_name.mat
+% load neccessary data if needed
+%load ../Weibodata.mat
+%load ../Active_users.mat
 
+% load the map data from /maps/bou2_4p.shp
 sheng = shaperead('maps/bou2_4p.shp', 'UseGeoCoords', true);
-for i = 1 : length(sheng)
-    sheng(i).NAME=sheng_chinese_name{i};
-end
 
+% replace GB2312 with utf-8
+load chinese_name.mat
+for i = 1 : length(sheng)
+    sheng(i).NAME = sheng_chinese_name{i};
+end
+clear sheng_chinese_name;
+
+% import the provinces and cities names existed in China from ex.txt
 d=importdata('ex.txt');
 ddata = d.data;
 textddata = d.textdata;
+clear d;
 
+% The 1st figure
+% calculate the sum of ACTIVE USERS of each provinces and cities
 pro_name = {...
     'Others', 'Zhejiang', 'Shanxi', 'Jiangxi', 'Shanghai', 'Tianjin',...
     'Fujian', 'Liaoning', 'Guangdong', 'Beijing', 'Shandong', 'Heilongjiang', ...
@@ -33,25 +44,30 @@ for i = 1 : length(ddata)
     ddata(i) = sum([Active_users.province] == j);
 end
 
+% Prepare for calculating the depth of color
 k = 128;
-mycolormap = summer(k);
-
+mycolormap = cool(k);
 geoname = {sheng.NAME};
 max_ddata = max(ddata);
 n = length(ddata);
-mysymbolspec = cell(1,n);
+mysymbolspec = cell(1, n);
+
+% calculate the depth of color
 for i = 1 : n
-    count=ddata(i);
-    mycoloridx=floor(k * count / max_ddata);
-    mycoloridx(mycoloridx < 1) = 1;
+    count = ddata(i);
+    mycoloridx = floor(k * count / max_ddata);
+    mycoloridx(mycoloridx < 1) = 1; %fill the blanks
+    
     myprovince = textddata{i};
     geoidx = strmatch(myprovince, geoname);
+    
     if numel(geoidx) > 0
-        province_name=geoname(geoidx(1));
+        province_name = geoname(geoidx(1));
         mysymbolspec{i} = {'NAME', char(province_name), 'FaceColor', mycolormap(mycoloridx, :)};
     end
 end
 
+% plot figure
 figure;
 subplot(1, 2 ,1);
 ax = worldmap('china');
@@ -59,21 +75,22 @@ setm(ax, 'grid', 'off');
 setm(ax, 'frame', 'off');
 setm(ax, 'parallellabel', 'off');
 setm(ax, 'meridianlabel', 'off');
-
 symbols = makesymbolspec('Polygon', {'default', 'FaceColor', [0.9 0.9 0.8],...
     'LineStyle', '--', 'LineWidth', 0.2, 'EdgeColor', [0.8 0.9 0.9]},...
     mysymbolspec{:});
 geoshow(sheng, 'SymbolSpec', symbols);
-
-colormap(summer(k));
+colormap(cool(k));
 hcb = colorbar('EastOutside');
 step = round(max_ddata / 11);
 set(hcb, 'YTick', (0 : 0.1 : 1));
 set(hcb, 'YTickLabel', num2cell(0 : step : max_ddata));
+title('The Distribution of Active users in China');
 
+% The 2nd figure
 % plot a china MAP and use the depth of color to illustrate the
 % distribution of ALL of the users
 
+% calculate the sum of ALL USERS of each provinces and cities
 for i = 1 : length(ddata)
     for j = 1 : length(pro_name)
         if strcmp(pro_name(j), textddata(i))
@@ -83,9 +100,11 @@ for i = 1 : length(ddata)
     ddata(i) = sum([data.province] == j);
 end
 
+% prepare for calculating the depth of color
 k = 128;
-mycolormap = summer(k);
+mycolormap = cool(k);
 
+% calculate the depth of color
 geoname = {sheng.NAME};
 max_ddata = max(ddata);
 n = length(ddata);
@@ -93,29 +112,31 @@ mysymbolspec = cell(1,n);
 for i = 1 : n
     count=ddata(i);
     mycoloridx=floor(k * count / max_ddata);
-    mycoloridx(mycoloridx < 1) = 1;
+    mycoloridx(mycoloridx < 1) = 1; %fill the blanks
+    
     myprovince = textddata{i};
     geoidx = strmatch(myprovince, geoname);
+    
     if numel(geoidx) > 0
-        province_name=geoname(geoidx(1));
+        province_name = geoname(geoidx(1));
         mysymbolspec{i} = {'NAME', char(province_name), 'FaceColor', mycolormap(mycoloridx, :)};
     end
 end
 
+% plot the figure
 subplot(1, 2 ,2);
 ax = worldmap('china');
 setm(ax, 'grid', 'off');
 setm(ax, 'frame', 'off');
 setm(ax, 'parallellabel', 'off');
 setm(ax, 'meridianlabel', 'off');
-
 symbols = makesymbolspec('Polygon', {'default', 'FaceColor', [0.9 0.9 0.8],...
     'LineStyle', '--', 'LineWidth', 0.2, 'EdgeColor', [0.8 0.9 0.9]},...
     mysymbolspec{:});
 geoshow(sheng, 'SymbolSpec', symbols);
-
-colormap(summer(k));
+colormap(cool(k));
 hcb = colorbar('EastOutside');
 step = round(max_ddata / 11);
 set(hcb, 'YTick', (0 : 0.1 : 1));
 set(hcb, 'YTickLabel', num2cell(0 : step : max_ddata));
+title('The Distribution of All users in China');
